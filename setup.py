5396b69ba55aac905a6d2f741e158fcfc4f17156
CoolDeveloper101 <49064764+CoolDeveloper101@users.noreply.github.com>
2019-11-20 12:36:17 +0530

    Creating setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 
 setup(
-  name = 'json2'
-  version = ''
+  name = 'json2',
+  version = '0.1.0',
+  description = 'This is a wrapper around the json module, which uses pickle to store a wide variety of objects.',
+  long_description = '''This is a wrapper around the json module, which uses pickle to store a wide variety of objects.
+Json is a nice format, but it does not let you store a custom data type. This module lets you store custom objects, which can be used in a wide variety of tasks. For example, you cannot store a numpy array using json but using this module, you can. Currently, it can only store dictionaries.''',
+  author = 'CoolDeveloper101',
+  url = 'https://github.com/CoolDeveloper101/json-2.0-python/',
+  packages = ['json2'],
 )
